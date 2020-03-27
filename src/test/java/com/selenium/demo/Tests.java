53e034ede2c011a65d379f76222185852d55ab71
tiago-mattos <tiagomattos.git@gmail.com>
2020-03-27 15:03:58 -0300

    Relatórios

--- src/test/java/com/selenium/demo/Tests.java
+++ src/test/java/com/selenium/demo/Tests.java
@@ -1,24 +1,28 @@
 package com.selenium.demo;
 
-import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.fail;
 
 import java.util.concurrent.TimeUnit;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.RepeatedTest;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
 import com.pages.PaginaCalculoPrecoPrazo;
 import com.pages.PaginaRastreamento;
-import com.utils.Utility;
 
 public class Tests {
+
+	public ExtentHtmlReporter htmlReporter;
+	public ExtentReports extent;
+	public ExtentTest logger;
+
 	PaginaRastreamento paginaRastreamento;
 	PaginaCalculoPrecoPrazo paginaCalculoPrecoPrazo;
 
@@ -35,52 +39,67 @@ public class Tests {
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
+		/* Relatório Teste */
+
+		htmlReporter = new ExtentHtmlReporter(System.getProperty("user.dir") + "/test-output/ExtentReport.html");
+		
+		// Create an object of Extent Reports
+		extent = new ExtentReports();
+		extent.attachReporter(htmlReporter);
+		extent.setSystemInfo("Host Name", "SoftwareTestingMaterial");
+		extent.setSystemInfo("Environment", "Production");
+		extent.setSystemInfo("User Name", "Rajkumar SM");
+		htmlReporter.config().setDocumentTitle("Extent reports");
+		// Name of the report
+		htmlReporter.config().setReportName("Relatório");
+
 	}
 
-	@Test
-	@Order(2)
-	@DisplayName("Cenário 2: Rastreamento por objeto inválido(falha)")
+	@RepeatedTest(value = 3)
+	@DisplayName("Rastreamento por objeto inválido")
 	void rastreamentoObjetoFalha() {
 		paginaRastreamento = new PaginaRastreamento(driver);
+		logger = extent.createTest("Rastreamento por objeto inválido");
+		logger.info("Teste Inicializado");
 		paginaRastreamento.abrirPaginaInicial();
 		paginaRastreamento.verificarCarregamentoPagina();
 		paginaRastreamento.informarObjeto("AA123456789BR");
 		paginaRastreamento.clicarBuscar();
 
-		// Utility.getScreenshotDate(driver, "Cenário_2");
-
 		try {
-			assertEquals("Aguardando postagem pelo remetente.", paginaRastreamento.getMensagemAlert(), "Teste");
-			System.out.println("Sucesso");
+			assertEquals("Aguardando postagem pelo remetente", paginaRastreamento.getMensagemAlert(), "Falha:");
+			logger.pass("Passou");
 		} catch (AssertionError e) {
-			fail("Falhou" + e.getMessage());
+			logger.fail("Teste" + e.getMessage());
+			System.out.println(e.getMessage());
 		}
 
 	}
 
-	@Test
-	@Order(2)
-	@DisplayName("Cenário 2: Rastreamento por objeto inválido(sucesso)")
-	void rastreamentoObjetoSuceso() {
+	@RepeatedTest(value = 3)
+	@DisplayName("Rastreamento por objeto inválido")
+	void rastreamentoObjetoScuess() {
 		paginaRastreamento = new PaginaRastreamento(driver);
+		logger = extent.createTest("Rastreamento por objeto inválido - OK");
+		logger.info("Teste Inicializado");
 		paginaRastreamento.abrirPaginaInicial();
 		paginaRastreamento.verificarCarregamentoPagina();
 		paginaRastreamento.informarObjeto("AA123456789BR");
 		paginaRastreamento.clicarBuscar();
 
-		// Utility.getScreenshotDate(driver, "Cenário_2");
-
 		try {
-			assertEquals("Aguardando postagem pelo remetente.", paginaRastreamento.getMensagemAlert(), "Teste");
-			System.out.println("Sucesso");
+			assertEquals("Aguardando postagem pelo remetente.", paginaRastreamento.getMensagemAlert(), "Falha:");
+			logger.pass("Passou");
 		} catch (AssertionError e) {
-			System.out.println("Erro; " + e.getMessage());
+			logger.fail(e.getMessage());
 		}
 
 	}
 
 	@AfterEach
 	void setAfter() {
+		logger.info("Teste Finalizado");
+		extent.flush();
 		driver.manage().deleteAllCookies();
 		driver.quit();
 
